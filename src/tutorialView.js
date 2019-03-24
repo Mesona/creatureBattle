b1f6cb9c3d09aa94cf8ae0ea791098a909cc568c
mesona <kylemesona@gmail.com>
2019-03-24 14:38:23 -0700

    Add tutorial view

--- src/tutorialView.js
+++ src/tutorialView.js
@@ -7,39 +7,28 @@ function TutorialView(game, ctx, canvas, gameView) {
   this.gameVeiw = gameView;
   this.offsetLeft = this.canvas.offsetLeft;
   this.offsetTop = document.getElementById('height-test').offsetTop; 
+  this.currentHighlight = ["creature", "showWeapon", "showArmor", "showBehavior", "showNext"];
 
   this.handleClick = this.handleClick.bind(this);
   this.handleCursor = this.handleCursor.bind(this);
   this.handleNext = this.handleNext.bind(this);
   this.handleSkip = this.handleSkip.bind(this);
+  this.showCreature = this.showCreature.bind(this);
+  this.showWeapon = this.showWeapon.bind(this);
+  this.showArmor = this.showArmor.bind(this);
+  this.showBehavior = this.showBehavior.bind(this);
+  this.showNext = this.showNext.bind(this);
+  this.clearView = this.clearView.bind(this);
+  this.writeTutorialNext = this.writeTutorialNext.bind(this);
+  this.writeTutorialSkip = this.writeTutorialSkip.bind(this);
 
   document.addEventListener('click', this.handleClick, false);
   document.addEventListener('mousemove', this.handleCursor, false);
-  this.ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
-  this.ctx.fillRect(0, 0, 800, 500)
 
-  this.ctx.clearRect(340, 280, 220, 200);
-  // ctx.fillStyle = "rgba(0, 0, 0, 0.8";
-  // ctx.fillRect(25, 425, 145, 45);
-  ctx.fillStyle = "rgba(255, 91, 1, 1)";
-
-  ctx.font = "italic 18pt Arial";
-  let lines = "Next";
-  ctx.fillText(getLines(ctx, lines, 50), 225, 450);
-
-  lines = "Skip Tutorial";
-  ctx.fillText(getLines(ctx, lines, 200), 25, 450);
-
-  ctx.font = "italic 25pt Arial";
-  lines = "This is your creature"
-  ctx.fillText(getLines(ctx, lines, 350), 20, 350);
-
-
+  this.showCreature();
 }
 
 TutorialView.prototype.handleClick = function(e) {
-  // let clickX = e.pageX - this.canvas.offsetLeft;
-  // let clickY = e.pageY - (document.getElementById('height-test').offsetTop);
   let clickX = e.pageX - this.offsetLeft;
   let clickY = e.pageY - this.offsetTop;
   console.log(`${clickX}, ${clickY}`)
@@ -49,13 +38,13 @@ TutorialView.prototype.handleClick = function(e) {
     // If the user clicks the Skip button
     if (clickX > 24 && clickX < 171) {
       this.handleSkip();      
+    // If the user clicks the Next button
     } else if (clickX > 224 && clickX < 296) {
-      console.log('correctNext')
+      // console.log('correctNext')
+      this.handleNext();
     }
   }
 
-  // call handleNext
-  // call handleSkip
 }
 
 TutorialView.prototype.handleCursor = function(e) {
@@ -69,6 +58,7 @@ TutorialView.prototype.handleCursor = function(e) {
       if (!this.canvas.classList.contains('cursor-pointer')) {
         this.canvas.classList.add('cursor-pointer')
       }
+    // If the user hovers over the Next button
     } else if (mouseX > 224 && mouseX < 296) {
       if (!this.canvas.classList.contains('cursor-pointer')) {
         this.canvas.classList.add('cursor-pointer')
@@ -82,6 +72,26 @@ TutorialView.prototype.handleCursor = function(e) {
 }
 
 TutorialView.prototype.handleNext = function(e) {
+  this.currentHighlight.shift();
+  switch (this.currentHighlight[0]) {
+    case "showWeapon":
+      this.showWeapon();
+      break;
+    case "showArmor":
+      this.showArmor();
+      break;
+    case "showBehavior":
+      this.showBehavior();
+      break;
+    case "showNext":
+      this.showNext();
+      break;
+    default:
+      this.handleSkip();
+      break;
+
+
+  }
 
   // document.removeEventListener("click", this.handleClick);
 }
@@ -93,6 +103,108 @@ TutorialView.prototype.handleSkip = function(e) {
   // new PreparationView(this.game, this.ctx, this.canvas, this.gameView).start();
   new PreparationView(this.game, this.ctx, this.canvas, this.gameView).start(); 
 };
+
+TutorialView.prototype.showCreature = function() {
+  this.ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+  this.ctx.fillRect(0, 0, 800, 500)
+
+  // this.ctx.clearRect(340, 280, 220, 200);
+  this.ctx.clearRect(340, 280, 220, 200);
+  this.ctx.fillStyle = "rgba(255, 91, 1, 1)";
+
+  // ctx.font = "italic 18pt Arial";
+  // let lines = "Next";
+  // ctx.fillText(getLines(ctx, lines, 50), 225, 450);
+
+  // lines = "Skip Tutorial";
+  // ctx.fillText(getLines(ctx, lines, 200), 25, 450);
+  this.writeTutorialNext();
+  this.writeTutorialSkip();
+
+  this.ctx.font = "italic 25pt Arial";
+  lines = "This is your creature"
+  this.ctx.fillText(getLines(this.ctx, lines, 350), 20, 350);
+}
+
+TutorialView.prototype.showWeapon = function() {
+  this.clearView(18, 17, 720, 60);
+
+  this.writeTutorialNext();
+  this.writeTutorialSkip();
+
+  this.ctx.font = "italic 25pt Arial";
+  lines1 = "This is where you";
+  lines2 = "select your weapon.";
+  this.ctx.fillText(getLines(this.ctx, lines1, 350), 20, 350);
+  this.ctx.fillText(getLines(this.ctx, lines2, 350), 20, 380);
+}
+
+TutorialView.prototype.showArmor = function() {
+  this.clearView(18, 110, 720, 60);
+
+
+  this.writeTutorialNext();
+  this.writeTutorialSkip();
+
+  this.ctx.font = "italic 25pt Arial";
+  lines1 = "This is where you";
+  lines2 = "select your armor.";
+  this.ctx.fillText(getLines(this.ctx, lines1, 350), 20, 350);
+  this.ctx.fillText(getLines(this.ctx, lines2, 350), 20, 380);
+
+}
+
+TutorialView.prototype.showBehavior = function() {
+  this.clearView(18, 205, 720, 60);
+
+
+  this.writeTutorialNext();
+  this.writeTutorialSkip();
+
+  this.ctx.font = "italic 25pt Arial";
+  lines1 = "This is where you";
+  lines2 = "select your behavior.";
+  this.ctx.fillText(getLines(this.ctx, lines1, 350), 20, 350);
+  this.ctx.fillText(getLines(this.ctx, lines2, 350), 20, 380);
+
+}
+
+TutorialView.prototype.showNext = function() {
+  this.clearView(595, 368, 190, 40);
+
+
+  this.writeTutorialNext();
+  this.writeTutorialSkip();
+
+  this.ctx.font = "italic 25pt Arial";
+  lines1 = "When ready, send";
+  lines2 = "your creature to battle.";
+  this.ctx.fillText(getLines(this.ctx, lines1, 350), 20, 350);
+  this.ctx.fillText(getLines(this.ctx, lines2, 350), 20, 380);
+
+}
+
+TutorialView.prototype.writeTutorialNext = function() {
+  this.ctx.fillStyle = "rgba(255, 91, 1, 1)";
+
+  this.ctx.font = "italic 18pt Arial";
+  let lines = "Next";
+  this.ctx.fillText(getLines(this.ctx, lines, 50), 225, 450);
+}
+
+TutorialView.prototype.writeTutorialSkip = function() {
+  lines = "Skip Tutorial";
+  this.ctx.fillText(getLines(this.ctx, lines, 200), 25, 450);
+}
+
+TutorialView.prototype.clearView = function(x, y, width, height) {
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  this.ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+  this.ctx.fillRect(0, 0, 800, 500)
+
+  this.ctx.clearRect(x, y, width, height);
+  // ctx.fillStyle = "rgba(255, 91, 1, 1)";
+}
 
 function getLines(ctx, text, maxWidth) {
   let words = text.split(" ");
